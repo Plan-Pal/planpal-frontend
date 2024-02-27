0d8980794e63d97306ce5175ba4168709210c11f
iiqcov <iiqcov@gmail.com>
2024-02-28 01:54:57 +0900

    [feat] 기본 색상 설정

---
+++ lib/themes/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryPurple = Color(0xFF4B2B80);
+  static const mediumPurple = Color(0xFF6F42BA);
+  static const lightPurple = Color(0xFF957ED9);
+  static const extraLightPurple = Color(0xFFF5F0F9);
+
+  static const primaryLime = Color(0xFFA2C835);
+
+  static const primaryRed = Color(0xFFD33030);
+  static const lightRed = Color(0xFFF05C5C);
+
+  static const darkGray = Color(0xFF383838);
+  static const gray = Color(0xFF979EAA);
+  static const lightGray = Color(0xFFC1C1C1);
+  static const extraLightGray = Color(0xFFF8F8F8);
+  static const white = Color(0xFFFFFFFF);
+}
