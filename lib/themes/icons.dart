d813616f5c4bfb807a3083cd4c8eaa6cabdf58b0
iiqcov <iiqcov@gmail.com>
2024-02-28 13:49:46 +0900

    [feat] 프로젝트 아이콘 추가

---
+++ lib/themes/icons.dart
@@ -0,0 +1,224 @@
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/widgets.dart';
+
+class AppIcon {
+  Widget getHomeIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/home-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getAddFriendsIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/add-friends-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getAddProfileIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/add-profile-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getCalendarIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/calendar-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getCheckIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/check-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getCoinIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/coin-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getDownArrowIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/down-arrow-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getFace1Icon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/face1-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getFace2Icon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/face2-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getFace3Icon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/face3-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getFace4Icon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/face4-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getFace5Icon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/face5-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getFace6Icon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/face6-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getLeftArrowIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/left-arrow-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getLocationIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/location-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getMailIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/mail-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getPlusIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/plus-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getProfileIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/profile-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getReloadIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/reload-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getRightArrowIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/right-arrow-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getSettingIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/setting-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget getTimeIcon(
+      {required Color color, required double height, required double width}) {
+    return SvgPicture.asset(
+      'assets/svg/time-icon.svg',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      height: height,
+      width: width,
+    );
+  }
+}
