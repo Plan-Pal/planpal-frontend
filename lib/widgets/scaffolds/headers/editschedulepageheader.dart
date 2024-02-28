4bc7b9cb898d0eac37d3ad6517d8213f8f5b02c0
iiqcov <iiqcov@gmail.com>
2024-02-28 21:24:03 +0900

    [feat] 약속 수정 페이지 헤더 추가

---
+++ lib/widgets/scaffolds/headers/editschedulepageheader.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:planpal_frontend/themes/colors.dart';
+import 'package:planpal_frontend/themes/fonts.dart';
+import 'package:planpal_frontend/themes/icons.dart';
+
+class EditSchedulePageHeader extends StatelessWidget
+    implements PreferredSizeWidget {
+  const EditSchedulePageHeader({Key? key})
+      : preferredSize = const Size.fromHeight(60.0),
+        super(key: key);
+
+  @override
+  final Size preferredSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: Text('약속 수정하기',
+          style:
+              AppFonts.interSemiBold(color: AppColors.darkGray, fontsize: 20)),
+      leading: IconButton(
+        icon: AppIcon()
+            .getBackIcon(color: AppColors.darkGray, height: 15, width: 18),
+        onPressed: () {},
+      ),
+      actions: const <Widget>[],
+    );
+  }
+}
