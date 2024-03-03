6232e0339942208ef31dfdd21d0584a51d8c5831
iiqcov <iiqcov@gmail.com>
2024-03-03 23:54:19 +0900

    [feat] 홈 스크린 구현

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:planpal_frontend/widgets/scaffolds/headers/homepageheader.dart';
+import '../../test/mock/mock_api_service.dart';
+import 'package:planpal_frontend/themes/colors.dart';
+import 'package:planpal_frontend/widgets/common/home_page/calendar/home_calendar.dart';
+import 'package:planpal_frontend/widgets/common/home_page/schedule/simple_schedule_info.dart';
+import 'package:planpal_frontend/widgets/common/home_page/schedule/home_shcedule_list_header.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime _selectedDate = DateTime.now();
+  List<InfoContainer> infoContainers = [];
+  List<Map<String, dynamic>> schedules = [];
+
+  void _onDateSelected(DateTime date) {
+    setState(() {
+      _selectedDate = date;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchAndSetSchedules();
+  }
+
+  Future<void> _fetchAndSetSchedules() async {
+    final apiService = MockApiService();
+    final scheduleData = await apiService.getSchedules();
+    final scheduleList = scheduleData['result']['schedules'] as List;
+
+    setState(() {
+      infoContainers = scheduleList.map((schedule) {
+        return InfoContainer(
+          title: schedule['title'],
+          time: schedule['appointed_time'],
+          location: schedule['place'],
+        );
+      }).toList();
+      schedules = List<Map<String, dynamic>>.from(scheduleList);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: HomePageHeader(),
-      body: const Center(
-        child: Text('홈 페이지'),
+      backgroundColor: AppColors.extraLightGray,
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: infoContainers.length + 2,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return CustomCalendar(
+                onDateSelected: _onDateSelected,
+                schedules: schedules,
+              );
+            } else if (index == 1) {
+              return AnotherWidget(selectedDate: _selectedDate);
+            } else {
+              DateTime scheduleTime =
+                  DateTime.parse(infoContainers[index - 2].time);
+              if (_selectedDate.year == scheduleTime.year &&
+                  _selectedDate.month == scheduleTime.month &&
+                  _selectedDate.day == scheduleTime.day) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: infoContainers[index - 2],
+                );
+              } else {
+                return Container();
+              }
+            }
+          },
+        ),
       ),
     );
   }
